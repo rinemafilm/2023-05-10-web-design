cbfccdb8714e9e295f62417583cd7cdef06b07aa
rinemafilm <rinamafilm@gmail.com>
2023-05-10 12:45:53 +0900

    a-1

---
+++ js/common.js
@@ -0,0 +1,40 @@
+$(function(){
+        //메뉴
+        $('.gnb').hover(
+            function(){
+                $('.gnb ul').stop().slideDown('fast')
+            },
+            function(){
+                $('.gnb ul').stop().slideUp('fast')
+            }
+        )
+
+        //슬라이드
+        setInterval(function(){
+            $('.slide').animate({top:-300}, function(){
+                $('.slide a:first').appendTo($('.slide'))
+                $('.slide').css({'marginTop':'0px'})
+            })
+        }, 3000)
+
+
+        //탭
+        console.log($('tap_title h2'))
+        $('.tap_title h2').click(function(){
+            $('.tap_title h2').removeClass('on')
+            $(this).addClass('on')
+
+            $('.tap_contents ul').css('display', 'none')
+            $('.tap_contents ul').eq($(this).index()).css('display', 'block')
+        })
+
+        //모달
+        $('.notice li:first').click(function(){
+            $('#modal').addClass('active')
+        })
+
+        $('.close').click(function(){
+            $('#modal').removeClass('active')
+        })
+   
+})
